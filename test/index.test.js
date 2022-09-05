5656ce7d20547717cbca4a41f50a46c9560b0a74
ramesh-choudhary-ext <ramesh.choudhary.ext@extendaretail.com>
2022-09-05 08:59:59 +0200

    feat: add unit test

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+describe("Inventory service Unit Tests", function () {
+    describe("Sell product", function () {
+      it("should successfully sell a product by adjusting the stock value for each product article.", async function () {
+      });
+      it("should throw an error if there is not sufficient stock for product article.", async function () {
+      });
+    });
+  });
